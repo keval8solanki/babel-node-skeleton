37505f5ff7ebb6c1cb57b4781f8800e2707a7341
Keval Solanki <57882119+keval8solanki@users.noreply.github.com>
2021-02-19 21:10:50 +0530

    Added Body Parser

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 const app = express()
 
-const PORT = 4000
+const PORT = 8080
+
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
